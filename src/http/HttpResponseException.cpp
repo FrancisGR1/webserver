95bb114d8bdc3b1139f64701b850a4f425df2830
francisco <146764998+FrancisGR1@users.noreply.github.com>
2026-02-21 11:05:38 +0000

    feat(http_response): dont include location in exceptions

--- src/http/HttpResponseException.cpp
+++ src/http/HttpResponseException.cpp
@@ -5,16 +5,9 @@
 
 HttpResponseException::HttpResponseException(StatusCode::Code code, const std::string& msg, const LocationConfig& lc)
 	: m_status(code)
-	, m_location(lc)
-	, m_msg(msg) {}
-
-HttpResponseException::HttpResponseException(StatusCode::Code code, const std::string& msg)
-	: m_status(code)
-	, m_location()
 	, m_msg(msg) {}
 
 StatusCode::Code HttpResponseException::status() const        { return m_status; }
-const LocationConfig& HttpResponseException::location() const { return m_location; }
 const std::string& HttpResponseException::msg() const         { return m_msg; }
 
 HttpResponseException::~HttpResponseException() throw() {}
--- src/http/HttpResponseException.hpp
+++ src/http/HttpResponseException.hpp
@@ -11,16 +11,13 @@ class ResponseError : public std::exception
 {
 	public:
 		explicit ResponseError(StatusCode::Code code, const std::string& msg);
-		explicit ResponseError(StatusCode::Code code, const std::string& msg, const LocationConfig& lc);
 		StatusCode::Code status() const;
-		const LocationConfig& location() const;
 		const std::string& msg() const;
 
 		virtual ~ResponseError() throw();
 
 	private:
 		StatusCode::Code m_status;
-		LocationConfig m_location; //@TODO isto deve ser um pointer
 		std::string m_msg;
 
 		// illegal
