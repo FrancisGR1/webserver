2094c3d9dde5f1477f4feabcb6448de0335274f5
francisco <146764998+FrancisGR1@users.noreply.github.com>
2026-03-10 16:38:07 +0000

    feat(server): implement connection pool

--- src/server/ConnectionPool.cpp
+++ src/server/ConnectionPool.cpp
@@ -1,64 +1,72 @@
+#include <iostream>
+
 #include "core/utils.hpp"
 #include "ConnectionPool.hpp"
 
 
-// make connection, store it, return a reference
-ConnectionManager& ConnectionPool::make(const Socket& conn_socket, EventManager& events)
+// make connection, store it, return a reference to it
+Connection& ConnectionPool::make(Socket& conn_socket, EventManager& events)
 {
-	if (utils::contains(conn_by_fd_, conn_socket.getFd()))
+	std::cout << "make()\n";
+	if (utils::contains(conn_by_fd_, conn_socket.fd()))
 		throw std::logic_error("Connection Pool: already contains this fd!\n");
+	std::cout << "contains() yes: " << conn_socket.fd() << "\n";
 
 	// create and save connection with its socket fd
-	connection_pool_.push_back(ConnectionManager(conn_socket, events));
-	conn_by_fd_[conn_socket.getFd()] = &connection_pool_.back();
-	return *conn_by_fd_[conn_socket.getFd()];
+	connection_pool_.insert(connection_pool_.end(), new Connection(conn_socket, events));
+	std::cout << "save conn\n";
+	conn_by_fd_[conn_socket.fd()] = connection_pool_.back();
+	return *conn_by_fd_[conn_socket.fd()];
 }
 
-void ConnectionPool::add_key_to_existing_connection(int key, ConnectionManager& conn)
+void ConnectionPool::associate_fd(int fd, Connection& conn)
 {
-	if (utils::contains(conn_by_fd_, key))
+	if (utils::contains(conn_by_fd_, fd))
 		throw std::logic_error("Connection Pool: already contains this fd!\n");
 
-	for (std::list<ConnectionManager>::iterator it = connection_pool_.begin();
+	for (std::list<Connection*>::iterator it = connection_pool_.begin();
 			it != connection_pool_.end(); ++it)
 	{
-		if (&(*it) == &conn)
+		if (*it == &conn)
 		{
 			throw std::logic_error("Connection Pool: connection not owned by pool!");
 		}
 	}
 
-	conn_by_fd_[key] = &conn;
+	conn_by_fd_[fd] = &conn;
 }
 
-void ConnectionPool::remove(ConnectionManager& conn)
+void ConnectionPool::remove(Connection& conn)
 {
-	// list
-	for (std::list<ConnectionManager>::iterator it = connection_pool_.begin();
-			it != connection_pool_.end(); )
-	{
-		if (&(*it) == &conn)
-		{
-			connection_pool_.erase(it);
-			break;
-		}
-	}
-
 	// map
-	for (std::map<int, ConnectionManager>::iterator it = conn_by_fd_.begin(); it != conn_by_fd_.end(); )
+	for (std::map<int, Connection*>::iterator it = conn_by_fd_.begin(); it != conn_by_fd_.end(); )
 	{
 		if (it->second == &conn)
 		{
-			it = conn_by_fd_.erase(it);
+			std::map<int, Connection*>::iterator tmp = it;
+			++it;
+			conn_by_fd_.erase(tmp);
 		}
 		else
 		{
 			++it;
 		}
 	}
+
+	// list
+	for (std::list<Connection*>::iterator it = connection_pool_.begin();
+			it != connection_pool_.end(); )
+	{
+		if (*it == &conn)
+		{
+			connection_pool_.erase(it);
+			break;
+		}
+	}
 }
 
-ConnectionManager& ConnectionPool::get(int fd)
+
+Connection& ConnectionPool::get(int fd)
 {
 	if (!utils::contains(conn_by_fd_, fd))
 		throw std::logic_error("Connection Pool: looking up fd that doesn't exist!\n");
--- src/server/ConnectionPool.hpp
+++ src/server/ConnectionPool.hpp
@@ -8,18 +8,19 @@
 class ConnectionPool
 {
 	public:
-		ConnectionManager& make(const Socket& conn_socket, EventManager& events);
-		void add_key_to_existing_connection(int key, ConnectionManager& conn);
+		Connection& make(Socket& conn_socket, EventManager& events);
+		void associate_fd(int fd, Connection& conn);
 
 		//@TODO: ainda não sei a melhor forma de remover?
-		void remove(ConnectionManager& conn);
+		void remove(Connection& conn);
+		void remove(int fd); //@TODO: impl
 
-		ConnectionManager& get(int fd);
+		Connection& get(int fd);
 
 	private:
 		// connections can have multiple associated ints
-		std::list<ConnectionManager> connection_pool_; // std::list so connection pointers are stable
-		std::map<int, ConnectionManager*>	conn_by_fd_;
+		std::list<Connection*> connection_pool_; // std::list so connection pointers are stable
+		std::map<int, Connection*>	conn_by_fd_;
 };
 
 #endif // CONNECTIONPOOL_HPP
