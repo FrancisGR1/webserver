f96885699f7c905fee27313e05cb63b8a5c62171
francisco <146764998+FrancisGR1@users.noreply.github.com>
2026-04-15 10:36:59 +0100

    refactor(server): EventManager uses EventAction instead of raw epoll_event

--- src/server/EventManager.cpp
+++ src/server/EventManager.cpp
@@ -1,59 +1,59 @@
 #include "EventManager.hpp"
-#include "core/EventAction.hpp"
 #include "core/Logger.hpp"
+#include "core/contracts.hpp"
+#include "server/EventAction.hpp"
+
+#include <sys/epoll.h>
+#include <unistd.h>
 
 #include <cstring>
 
-EventManager::EventManager()
+EventManager::EventManager(size_t max_events)
     : m_epoll_fd(-1)
+    , m_max_events(max_events)
+    , m_events(new epoll_event[max_events])
 {
+    Logger::trace("EventManager: constructor");
+
     m_epoll_fd = epoll_create(1);
     if (m_epoll_fd == -1)
-        Logger::error("EventManager: Failed to create epoll fd!");
+    {
+        throw std::runtime_error("EventManager: Failed to create epoll fd!");
+    }
 }
 
 EventManager::~EventManager()
 {
+    Logger::trace("EventManager: destructor");
+
     if (m_epoll_fd != -1)
         close(m_epoll_fd);
+
+    delete[] m_events;
 }
 
-const epoll_event& EventManager::get_event(int index) const
+//@TODO @REFACTOR: should return an event action isntead of an epoll_event
+// epoll_events should only live in EventManager
+EventAction EventManager::get_event(size_t index) const
 {
+    REQUIRE(index < m_max_events, "Out of bounds!");
+
     Logger::trace("EventManager: Getting event [%d]:%d", index, m_events[index].data.fd);
-    return (m_events[index]);
+
+    return (EventAction(m_events[index]));
+}
+
+void EventManager::apply(const EventAction& ea)
+{
+    to_epoll_event(ea, ea.conn);
 }
 
 void EventManager::apply(const std::vector<EventAction>& actions, Connection* ctx)
 {
     for (size_t i = 0; i < actions.size(); ++i)
     {
-        const EventAction& a = actions[i];
-        epoll_event ev;
-        ev.data.ptr = ctx;
-
-        switch (a.action)
-        {
-            case EventAction::WantReading:
-                ev.events = EPOLLIN;
-                epoll_ctl(m_epoll_fd, is_tracked(a.fd) ? EPOLL_CTL_MOD : EPOLL_CTL_ADD, a.fd, &ev);
-                break;
-            case EventAction::WantWriting:
-                ev.events = EPOLLOUT;
-                epoll_ctl(m_epoll_fd, is_tracked(a.fd) ? EPOLL_CTL_MOD : EPOLL_CTL_ADD, a.fd, &ev);
-                break;
-            case EventAction::WantClose:
-                epoll_ctl(m_epoll_fd, EPOLL_CTL_DEL, a.fd, NULL);
-                close(a.fd);
-                untrack(a.fd);
-                break;
-            case EventAction::WantDelete:
-                epoll_ctl(m_epoll_fd, EPOLL_CTL_DEL, a.fd, NULL);
-                untrack(a.fd);
-                break;
-            case EventAction::Stay: // do nothing
-                break;
-        }
+        const EventAction& ea = actions[i];
+        to_epoll_event(ea, ctx);
     }
 }
 
@@ -85,6 +85,7 @@ int EventManager::remove(int socket)
     return ret;
 }
 
+// utils
 bool EventManager::is_tracked(int fd)
 {
     return m_tracked_fds.count(fd) > 0;
@@ -93,4 +94,29 @@ bool EventManager::is_tracked(int fd)
 void EventManager::untrack(int fd)
 {
     m_tracked_fds.erase(fd);
+}
+void EventManager::to_epoll_event(const EventAction& ea, Connection* ctx)
+{
+    epoll_event ev;
+    ev.data.ptr = ctx;
+
+    switch (ea.action)
+    {
+            //@TODO apanhar possível error de epoll_ctl
+        case EventAction::WantReading:
+            ev.events = EPOLLIN;
+            epoll_ctl(m_epoll_fd, is_tracked(ea.fd) ? EPOLL_CTL_MOD : EPOLL_CTL_ADD, ea.fd, &ev);
+            break;
+        case EventAction::WantWriting:
+            ev.events = EPOLLOUT;
+            epoll_ctl(m_epoll_fd, is_tracked(ea.fd) ? EPOLL_CTL_MOD : EPOLL_CTL_ADD, ea.fd, &ev);
+            break;
+        case EventAction::WantClose:
+            epoll_ctl(m_epoll_fd, EPOLL_CTL_DEL, ea.fd, NULL);
+            close(ea.fd); //@WARN @ERROR é suposto fechar aqui?
+            untrack(ea.fd);
+            break;
+        case EventAction::WantNothing: // do nothing
+            break;
+    }
 }
--- src/server/EventManager.hpp
+++ src/server/EventManager.hpp
@@ -4,34 +4,34 @@
 #include <set>
 #include <vector>
 
-#include <sys/epoll.h>
-#include <unistd.h>
-
-#include "core/EventAction.hpp"
+#include "server/EventAction.hpp"
 
 class EventManager
 {
   public:
     // construct/destruct
-    EventManager();
+    EventManager(size_t max_events);
     ~EventManager();
 
     // api
-    const epoll_event& get_event(int index) const;
+    EventAction get_event(size_t index) const;
     void apply(const std::vector<EventAction>& actions, Connection* ctx);
+    void apply(const EventAction& actions);
     int wait();
     int add(int fd, uint32_t events);
     int remove(int fd);
 
   private:
     int m_epoll_fd;
-    // events are either sockets, files or pipes
-    epoll_event m_events[1024];
+    // an event is one of the following: socket, file, pipe
+    size_t m_max_events;
+    epoll_event* m_events;
     std::set<int> m_tracked_fds;
 
     // utils
     bool is_tracked(int fd);
     void untrack(int fd);
+    void to_epoll_event(const EventAction& ea, Connection* ctx);
 };
 
 #endif /* EVENT_MANAGER */
