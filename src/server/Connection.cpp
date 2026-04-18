091434b6ff48244ad43c17550fe9f0f0a0052d66
francisco <146764998+FrancisGR1@users.noreply.github.com>
2026-04-18 21:20:27 +0100

    feat(connection): fast path
    * Prefer fast path for requests where the process() is made in one call
    * "Fast path" here means the connection skips the intermediary phase of
    processing the request and goes directly to the writing phase.

--- src/server/Connection.cpp
+++ src/server/Connection.cpp
@@ -23,105 +23,6 @@ Connection::~Connection()
     Logger::trace("Connection: destructor");
 }
 
-// @QUESTION é possível haver um mismatch do evento e do estado?
-// por exemplo, a ligação pode estar no estado de escrita e receber um evento de epollin, ou seja, quanddo a ligação
-// estiver a enviar, o cliente pde enviar ao mesmo tempo? tudo o que está abaixo assume que o cliente respeita/dá tempo
-// à ligação
-void Connection::handle_event(const EventAction& action)
-{
-    //@TODO: check if event matches connection state
-    (void)action;
-
-    switch (m_state)
-    {
-        case Reading:
-        {
-            Logger::trace("Connection: state - Receiving");
-
-            // read client info to buffer
-            char buffer[constants::read_chunk_size + 1];
-            ssize_t bytes = recv(m_socket.fd(), buffer, constants::read_chunk_size, 0);
-            if (bytes == 0)
-            {
-                Logger::info("Connection: Client closed connection on fd %d", m_socket.fd());
-                m_state = Done;
-                return;
-            }
-            if (bytes == -1)
-            {
-                Logger::warn("Connnection: recv() returned -1: errno says: '%s'", strerror(errno));
-                //@TODO substituir por timeout, se timeout = yes, então done, caso contrário não
-                if (errno == EAGAIN || errno == EWOULDBLOCK)
-                {
-                    // Trace instead of Warn: this is a normal part of non-blocking I/O
-                    Logger::trace("Connection: recv() EAGAIN on fd %d (waiting for data)", m_socket.fd());
-                    return;
-                }
-                Logger::error("Connection: fatal recv() error on fd %d: %s", m_socket.fd(), strerror(errno));
-                m_state = Done; // Only set Done on ACTUAL errors
-                return;
-            }
-
-            // parser works on raw strings but just to be safe...
-            buffer[bytes] = '\0';
-
-            // parse
-            m_parser.feed(buffer);
-
-            //@TODO juntar isto à fase de sending
-            // sending -> processing
-            // o evento deve ser mudado para EPOLLOUT na altura de
-            // escrever a resposta e não como está agora
-            // (na altura de GET/POST de ficheiro)
-            if (m_parser.done())
-            {
-                const Request& request_ = m_parser.get();
-                Logger::debug_obj(request_, "Connection: Request: ");
-                m_processor.set(request_);
-                m_events.push_back(
-                    EventAction(EventAction::WantProcessRequest, EventAction::ClientSocket, m_socket.fd(), this));
-                m_state = ProcessingRequest;
-            }
-            break;
-        }
-        case ProcessingRequest:
-        {
-            Logger::trace("Connection: state - Processing");
-            m_processor.process();
-
-            if (m_processor.done())
-            {
-                Logger::trace("Connection: RequestProcessor: Done!");
-                m_response = m_processor.response();
-                Logger::debug_obj(m_response, "Connection: Response:\n");
-                m_events.push_back(EventAction(EventAction::WantWrite, EventAction::ClientSocket, m_socket.fd(), this));
-                m_state = Writing;
-            }
-            break;
-        }
-        case Writing:
-        {
-            Logger::trace("Connection: state - Sending");
-
-            // write to socket
-            m_response.send(m_socket.fd());
-
-            if (m_response.done())
-            {
-                Logger::debug_obj(m_response, "Connection: Response:\n");
-                Logger::info("Connection: state - done!");
-
-                // close client socket by DEFAULT (http 1.0)
-                m_events.push_back(EventAction(EventAction::WantClose, EventAction::ClientSocket, m_socket.fd(), this));
-
-                m_state = Done;
-            }
-            break;
-        }
-        default: break;
-    }
-}
-
 std::vector<EventAction> Connection::give_events()
 {
 
@@ -181,19 +82,22 @@ void Connection::read()
     // parse
     m_parser.feed(buffer);
 
-    //@TODO juntar isto à fase de sending
-    // sending -> processing
-    // o evento deve ser mudado para EPOLLOUT na altura de
-    // escrever a resposta e não como está agora
-    // (na altura de GET/POST de ficheiro)
     if (m_parser.done())
     {
         const Request& request_ = m_parser.get();
         Logger::debug_obj(request_, "Connection: Request: ");
         m_processor.set(request_);
-        m_events.push_back(
-            EventAction(EventAction::WantProcessRequest, EventAction::ClientSocket, m_socket.fd(), this));
         next_state(ProcessingRequest);
+
+	// fast path -> process immediately
+	process_request();
+	if (!m_processor.done())
+	{
+        	m_events.push_back(
+        	    EventAction(EventAction::WantProcessRequest, EventAction::ClientSocket, m_socket.fd(), this));
+	}
+
+	Logger::trace("Connection: on fast path");
     }
 }
 
