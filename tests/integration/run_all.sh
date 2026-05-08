148c21b45176b926001f9e76641f6df06f9ef429
francisco <146764998+FrancisGR1@users.noreply.github.com>
2026-05-08 18:01:51 +0100

    test: print server pid and dont kill

--- tests/integration/run_all.sh
+++ tests/integration/run_all.sh
@@ -4,11 +4,10 @@ set -e
 # start the webserver in background
 ./webserv.out config/... &
 SERVER_PID=$!
+echo "Server pid: '$$'"
 sleep 1  # give it time to start
 
 lua5.4 quick.lua
 lua5.4 big.lua
 go test ./...
 ./stress/siege_test.sh
-
-kill $SERVER_PID
